54d2b29b42d88d1363d486dd844e55769999ddea
gerg <gerrrrrrrg@gmail.com>
2021-09-22 10:45:21 -0400

    first commit

---
+++ getPools.py
@@ -0,0 +1,50 @@
+# python basics
+import os
+import sys
+import json
+
+# balpy subgraph module
+import balpy.graph.graph as balGraph
+
+def main():
+	
+	batch_size = 30;
+	networks = ["mainnet", "kovan", "polygon", "arbitrum"];
+
+	poolsPath = "./pools/";
+	
+	for network in networks:
+
+		print();
+
+		# initialize subgraph
+		bg = balGraph.TheGraph(network);
+		try:
+			# query subgraph
+			pools = bg.getV2PoolIDs(batch_size);
+			
+			# only save this data if there is no file, or if the new data has more pools than the old data
+			poolFilePath = poolsPath + network + ".json";
+			saveData = False;
+			if os.path.isfile(poolFilePath):
+				with open(poolFilePath, 'r') as f:
+					oldPools = json.load(f);
+					if oldPools["header"]["numPools"] < pools["header"]["numPools"]:
+						saveData = True;
+			else:
+				saveData = True;
+
+			# save data to file, nicely formatted with indents
+			if saveData:
+				print("Saving", network, "data to", poolFilePath);
+				with open(poolFilePath, 'w') as f:
+					json.dump(pools, f, indent=4);
+			else:
+				print("No new pools, skipping", network);
+		
+		# catch errors if subgraph is down or internet connection bad
+		except:
+			print("Unexpected error:", sys.exc_info()[0]);
+
+if __name__ == '__main__':
+	main();
